e2e696200a62edad1d8eca3da07b68d8d880b030
Justin Franzen <justinafranzen@gmail.com>
2024-09-10 13:41:32 -0500

    Update config

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from nicegui import native, ui
 from pathlib import Path
-import configparser
+import json
 
-_config_file_ = Path.home().joinpath("bfcontrol.toml")
+CONFIG_FILE = Path.home().joinpath("bfcontrol.json")
 
 class Lights:
     def __init__(self):
@@ -15,28 +15,34 @@ class Lights:
 
 class Settings:
     def __init__(self):
-        if(not _config_file_.exists()):
+        if(not CONFIG_FILE.exists()):
             self._create_config()
-        config = configparser.ConfigParser()
-        config.sections()
-        self.config = config.read(_config_file_.as_posix())
+        with open(CONFIG_FILE.as_posix(), 'r') as openfile:
+            self.config = json.load(openfile)
+    
     def _create_config(self):
-        config = configparser.ConfigParser()
-        config["light_ids"] =[1,2]
-        config["nlight_ip"] ="192.168.1.5"
-        config["auth_header"]= "SuperSecret"
+        config = {
+            "light_ids": [1,2],
+            "nlight_ip": "192.168.1.5",
+            "auth_header": "SuperSecret",
+            "worship_lvl": 1.0,
+            "teaching_lvl": 60.0,
+        }
+        with open(CONFIG_FILE.as_posix(), "w") as outfile:
+            outfile.write(json.dumps(config, indent=4))
         
 dark = ui.dark_mode().enable()
 config = Settings().config
 lights = Lights()
 
+print(config)
 ui.label('Lighting Control')
 
 with ui.button_group():
     ui.button('Off',on_click=lambda:(lights.update_brightness(0.0)))
     ui.button('On', on_click=lambda:(lights.update_brightness(100.0)))
-    ui.button('Worship/Prayer', on_click=lambda:(lights.update_brightness(1.0)))
-    ui.button('Teaching', on_click=lambda: (lights.update_brightness(60.0)))
+    ui.button('Worship/Prayer', on_click=lambda:(lights.update_brightness(config["worship_lvl"])))
+    ui.button('Teaching', on_click=lambda: (lights.update_brightness(config["teaching_lvl"])))
 
 with ui.row():
     ui.label("Dimmer:")
